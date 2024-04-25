fa3d425cd99fdb75fb281ebb7e5c3512aa63a046
Артемий <artema121rus@mail.ru>
2024-04-25 18:51:09 +0300

    State, reducers, redux hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { type } from "@testing-library/user-event/dist/type";
+import { useDispatch, useSelector } from "react-redux";
+
 function App() {
-    return <div className='App'>проект</div>;
+    // useDispatch нужен для dispatch - изменяет состояние
+    // useSelector нужен для полученя состояния
+    const dispatch = useDispatch();
+    const cash = useSelector((state) => state.cash);
+
+    const getCash = (cash) => {
+        dispatch({ type: "ADD_CASH", payload: cash });
+    };
+    const addCash = (cash) => {
+        dispatch({ type: "GET_CASH", payload: cash });
+    };
+
+    return (
+        <div className='App'>
+            <div style={{ fontSize: "40px" }}>{cash}</div>
+            <div>
+                <button onClick={() => getCash(Number(prompt()))}>
+                    Снять деньги
+                </button>
+                <button onClick={() => addCash(Number(prompt()))}>
+                    Положить деньги
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const defaultState = {
+    cash: 0,
+};
+
+const reducer = (state = defaultState, actions) => {
+    switch (actions.type) {
+        case "ADD_CASH":
+            return { ...state, cash: state.cash - actions.payload };
+        case "GET_CASH":
+            return { ...state, cash: state.cash + actions.payload };
+        default:
+            return state;
+    }
+};
+
+// store - объект, содержащий методы: getstate - получить состояние, dispatch - изменить состояние, и подписаться на изменение. Передаем туда reducer
+const store = createStore(reducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    // Provider нужен для того, чтобы опрокидывать состояние в компоненты
+    <Provider store={store}>
         <App />
-    </React.StrictMode>
+    </Provider>
 );
