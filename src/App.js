b6bc835f279a6fb8ee3ce76f0f22768a7a769b6c
Артемий <artema121rus@mail.ru>
2024-04-26 09:44:09 +0300

    React и Redux.Action creators. Работа с массивами. Рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
+import {
+    addCustomerAction,
+    removeCustomerAction,
+} from "./store/customersReducer";
+import { addCashAction, getCashAction } from "./store/cashReducer";
 
 function App() {
     // useDispatch нужен для dispatch - изменяет состояние
     // useSelector нужен для полученя состояния
     const dispatch = useDispatch();
     const cash = useSelector((state) => state.cash.cash);
+    const customers = useSelector((state) => state.customers.customers);
 
     const addCash = (cash) => {
-        dispatch({ type: "ADD_CASH", payload: cash });
+        dispatch(addCashAction(cash));
     };
 
     const getCash = (cash) => {
-        dispatch({ type: "GET_CASH", payload: cash });
+        dispatch(getCashAction(cash));
+    };
+
+    const addCustomer = (name) => {
+        const customer = {
+            name,
+            id: Date.now(),
+        };
+        dispatch(addCustomerAction(customer));
+    };
+
+    const removeCustomer = (customer) => {
+        dispatch(removeCustomerAction(customer.id));
     };
 
     return (
@@ -24,7 +42,22 @@ function App() {
                 <button onClick={() => addCash(Number(prompt()))}>
                     Положить деньги
                 </button>
+                <button onClick={() => addCustomer(prompt())}>
+                    Добавить клиента
+                </button>
             </div>
+            {customers.length > 0 ? (
+                customers.map((customer) => (
+                    <div
+                        onClick={() => removeCustomer(customer)}
+                        key={customer.id}
+                    >
+                        {customer.name}
+                    </div>
+                ))
+            ) : (
+                <div>Клиенты не найдены!</div>
+            )}
         </div>
     );
 }
--- src/store/customersReducer.js
+++ src/store/customersReducer.js
@@ -2,13 +2,33 @@ const defaultState = {
     customers: [],
 };
 
+const ADD_CUSTOMER = "ADD_CUSTOMER";
+const REMOVE_CUSTOMER = "REMOVE__CUSTOMER";
+
 export const customersReducer = (state = defaultState, actions) => {
     switch (actions.type) {
-        case "ADD_CASH":
-            return { ...state, cash: state.cash + actions.payload };
-        case "GET_CASH":
-            return { ...state, cash: state.cash - actions.payload };
+        case ADD_CUSTOMER:
+            return {
+                ...state,
+                customers: [...state.customers, actions.payload],
+            };
+        case REMOVE_CUSTOMER:
+            return {
+                ...state,
+                customers: state.customers.filter(
+                    (customer) => customer.id !== actions.payload
+                ),
+            };
         default:
             return state;
     }
 };
+
+export const addCustomerAction = (payload) => ({
+    type: ADD_CUSTOMER,
+    payload,
+});
+export const removeCustomerAction = (payload) => ({
+    type: REMOVE_CUSTOMER,
+    payload,
+});
